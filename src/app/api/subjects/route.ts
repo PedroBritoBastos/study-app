e7b0c886ea7242a747b39f72da84f6daae183272
pedro <pedrobritobastos234@gmail.com>
2026-02-03 11:49:49 -0300

    api alterada para criação de usuarios

--- src/app/api/subjects/route.ts
+++ src/app/api/subjects/route.ts
@@ -1,17 +1,27 @@
 import { NextResponse, NextRequest } from "next/server";
 import { prisma } from "../../../../prisma/prisma";
+import { getUserFromToken } from "../_helpers/getUserByToken";
 
 // api/subjects get
 export async function GET() {
   try {
+    const user = await getUserFromToken();
+
+    if (!user) {
+      return NextResponse.json({ error: "Não autorizado" }, { status: 401 });
+    }
+
     const subjects = await prisma.subject.findMany({
+      where: {
+        userId: user.id,
+      },
       orderBy: {
         currentDate: "desc",
       },
     });
 
     return NextResponse.json(subjects);
-  } catch (error) {
+  } catch {
     return NextResponse.json(
       { error: "Erro ao buscar conteúdos" },
       { status: 500 },
@@ -22,6 +32,12 @@ export async function GET() {
 // api/subjects post
 export async function POST(request: NextRequest) {
   try {
+    const user = await getUserFromToken();
+
+    if (!user) {
+      return NextResponse.json({ error: "Não autorizado" }, { status: 401 });
+    }
+
     const body = await request.json();
     const { title, content } = body;
 
@@ -29,6 +45,7 @@ export async function POST(request: NextRequest) {
       data: {
         title,
         content,
+        userId: user.id,
       },
     });
 
