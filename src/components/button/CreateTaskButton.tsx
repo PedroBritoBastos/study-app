6be9e5ab51abea57b918758d3775386cd63b34ec
pedro <pedrobritobastos234@gmail.com>
2026-02-08 18:25:38 -0300

    CreateTaskButton: campo para adicionar tarefa

--- src/components/button/CreateTaskButton.tsx
+++ src/components/button/CreateTaskButton.tsx
@@ -1,13 +1,31 @@
-import { Button, Stack } from "@chakra-ui/react";
+"use client"
+
+import { Button, Stack, Field, Input, Flex } from "@chakra-ui/react";
 import { Plus } from "lucide-react";
 
 import { styles } from "@/styles/button/createTaskButton.styles";
 
+import { useState } from "react";
+
 export function CreateTaskButton() {
-  return <Stack>
-    <Button {...styles.button}>
+  const [open, setOpen] = useState(false);
+
+  return <Stack {...(open && styles.open)}>
+    <Button {...styles.button} onClick={() => setOpen(!open)}>
       <Plus />
       Nova Tarefa
     </Button>
+
+    {open &&
+      <Stack {...styles.createTaskContainer}>
+        <Field.Root>
+          <Field.Label {...styles.createTaskLabel}>Título</Field.Label>
+          <Flex {...styles.createTaskInputContainer}>
+            <Input {...styles.createTaskInput} />
+            <Button {...styles.createTaskAddButton} onClick={() => setOpen(!open)}>Adicionar</Button>
+          </Flex>
+        </Field.Root>
+      </Stack>
+    }
   </Stack>
 }
