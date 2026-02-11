d91b9e3faf91663d9ff3883401abf1a001c6f970
pedro <pedrobritobastos234@gmail.com>
2026-02-11 19:38:08 -0300

    goalsSidebar: problema com as tasks resolvido

--- src/components/sidebar/GoalsSidebar.tsx
+++ src/components/sidebar/GoalsSidebar.tsx
@@ -9,7 +9,9 @@ import { Text, Stack } from "@chakra-ui/react";
 import { styles } from "@/styles/sidebar/goalsSidebar.styles";
 
 import { useGoalsSidebar } from "@/src/hooks/goalClient/useGoalsSidebar";
+import { useState, useEffect } from "react";
 
+import { getTasks } from "@/src/services/taskService";
 
 interface Props {
   closeSidebar: () => void;
@@ -17,14 +19,38 @@ interface Props {
 }
 
 export function GoalsSidebar({ closeSidebar, goal }: Props) {
-  const goalsSidebarHook = useGoalsSidebar(goal);
+
+  // estado para guardar as tasks do goal atual
+  const [goalTasks, setGoalTasks] = useState(goal.tasks);
+
+  // estado para monitorar quando uma nova tarefa é adicionada
+  const [addedTask, setAddedTask] = useState({});
+
+  function handleAddTask(task) {
+    setAddedTask(task);
+  }
+
+  // toda vez que goal mudar, ou uma nova tarefa é adicionada, o componente deve re-renderizar
+  useEffect(() => {
+    setGoalTasks(goal.tasks);
+  }, [goal])
+
+  // quando addedTask mudar, o componente deve fazer um fetch para pegar as tarefas
+  useEffect(() => {
+    async function fetchTasks() {
+      const response = await getTasks(goal.id);
+      setGoalTasks(response);
+    }
+    fetchTasks();
+  }, [addedTask]);
+
 
   return <SidebarContainer header={goal.title} closeSidebar={closeSidebar}>
     {/* tasks em andamento */}
     <Text {...styles.statusText}>Em andamento</Text>
     <Stack {...styles.tasksStack}>
-      {goalsSidebarHook.tasks.map((task) => (<Text {...styles.task} key={task.id}>{task.title}</Text>))}
-      <CreateTaskButton goalId={goal.id} updateAddedTask={goalsSidebarHook.updateAddedTask} />
+      {goalTasks.map((task) => (<Text {...styles.task} key={task.id}>{task.title}</Text>))}
+      <CreateTaskButton goalId={goal.id} updateAddedTask={handleAddTask} />
     </Stack>
   </SidebarContainer>
 }
