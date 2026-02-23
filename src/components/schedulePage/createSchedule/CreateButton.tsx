8e3685c8b564c2805849cb108b7c8d16a6184752
pedro <pedrobritobastos234@gmail.com>
2026-02-23 07:51:00 -0300

    logica de criar tasks visualmente feita

--- src/components/schedulePage/createSchedule/CreateButton.tsx
+++ src/components/schedulePage/createSchedule/CreateButton.tsx
@@ -21,9 +21,28 @@ const styles = {
 export function CreateButton() {
 
   const [scheduleDate, setScheduleDate] = useState(""); // data do cronograma
-  const [tasks, setTasks] = useState([]); // lista de tarefas
+  const [tasks, setTasks] = useState<{ name: string, endTime: string }[]>([]); // lista de tarefas
   const [taskName, setTaskName] = useState(""); // nome da tarefa a ser criada
   const [taskEndTime, setTaskEndTime] = useState(""); // horário de término da tarefa a ser criada
+
+  const handleAddTask = () => {
+    const task = {
+      name: taskName,
+      endTime: taskEndTime
+    }
+
+    const tasklist = [...tasks, task];
+    setTasks(tasklist);
+    setTaskName("");
+    setTaskEndTime("");
+  }
+
+  const handleClose = () => {
+    setTasks([]);
+    setTaskName("");
+    setTaskEndTime("");
+    setScheduleDate("");
+  }
 
   return (
     <Dialog.Root size="md" placement="center" motionPreset="slide-in-bottom" >
@@ -42,35 +61,61 @@ export function CreateButton() {
             <Dialog.Header>
               <Dialog.Title>Novo cronograma</Dialog.Title>
               <Dialog.CloseTrigger asChild>
-                <CloseButton size="sm" />
+                <CloseButton
+                  size="sm"
+                  onClick={handleClose}
+                />
               </Dialog.CloseTrigger>
             </Dialog.Header>
 
             <Dialog.Body>
               <Field.Root mb={5}>
                 <Field.Label>Data</Field.Label>
-                <Input type="date" w={"fit-content"} />
+                <Input
+                  type="date"
+                  w={"fit-content"}
+                  value={scheduleDate}
+                  onChange={(e) => setScheduleDate(e.target.value)}
+                />
               </Field.Root>
 
               <Flex mb={5} alignItems={"center"} justifyContent={"space-between"} gap={4}>
                 <Field.Root flex={3}>
                   <Field.Label>Criar tarefa</Field.Label>
-                  <Input type="text" placeholder="nome da tarefa" />
+                  <Input
+                    type="text"
+                    placeholder="nome da tarefa"
+                    value={taskName}
+                    onChange={(e) => setTaskName(e.target.value)}
+                  />
                 </Field.Root>
 
                 <Field.Root width={"fit-content"} flex={1}>
                   <Field.Label>{"Horário (opcional)"}</Field.Label>
-                  <Input type="time" />
+                  <Input
+                    type="time"
+                    value={taskEndTime}
+                    onChange={(e) => setTaskEndTime(e.target.value)}
+                  />
                 </Field.Root>
               </Flex>
 
               {/* Botão para adicionar tarefas */}
-              <Button bg={"purple.600"} _hover={{ bg: "purple.500" }} size="sm" mb={5}>
+              <Button
+                bg={"purple.600"}
+                _hover={{ bg: "purple.500" }}
+                size="sm"
+                mb={5}
+                onClick={handleAddTask}
+              >
                 Adicionar tarefa
               </Button>
 
               {/* tarefas adicionadas */}
               <Stack h={"300px"} bg={"gray.200"} borderRadius={"md"} p={3} overflowY={"auto"}>
+                {tasks.map((task, index) => (
+                  <Task key={index} name={task.name} endTime={task.endTime} />
+                ))}
               </Stack>
             </Dialog.Body>
 
